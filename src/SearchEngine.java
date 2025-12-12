8937291441c12856c23613f30ff0c5c9ad044b34
kle-di <kledi.hasa63@gmail.com>
2025-12-12 23:57:32 +0100

    Restructured the project

--- src/SearchEngine.java
+++
@@ -1,7 +0,0 @@
-public class SearchEngine {
-
-    public void searchMaterials(String query) {}
-    public void searchVideos(String query) {}
-    public void searchDecks(String query) {}
-    public void searchGroups(String query) {}
-}
