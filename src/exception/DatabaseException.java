34afde8a40eaa895c31d7ccf285cc0be46305252
kle-di <kledi.hasa63@gmail.com>
2025-12-13 17:58:52 +0100

    Added database connector classes

--- src/exception/DatabaseException.java
+++ src/exception/DatabaseException.java
@@ -1,4 +1,9 @@
 package exception;
 
-public class DatabaseException {
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
