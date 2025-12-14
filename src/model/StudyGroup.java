a33098ee3ef1a69eb546572e390450047be999a4
fjonna <fjonahasani4@gmail.com>
2025-12-14 17:05:15 +0100

    Add User and StudyGroup core models

--- src/model/StudyGroup.java
+++ src/model/StudyGroup.java
@@ -1,4 +1,30 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public class StudyGroup {
+
+    private String id;
+    private String name;
+    private Set<String> memberIds;
+
+    public StudyGroup(String name) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.memberIds = new HashSet<>();
+    }
+
+    public void addMember(User user) {
+        memberIds.add(user.getId());
+    }
+
+    public boolean isMember(User user) {
+        return memberIds.contains(user.getId());
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,32 @@
 package model;
 
+import java.util.UUID;
+
 public class User {
+
+    private String id;
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void updateName(String newName) {
+        this.name = newName;
+    }
 }
